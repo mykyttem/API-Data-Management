1ab248d8bca5086f9a8dfc41803ab35d8c2ea686
chamois1 <nrabicenko5@gmail.com>
2024-04-03 16:36:17 +0300

    backend: improve code for reload data

--- backend/parsing_requests/random_user.py
+++ backend/parsing_requests/random_user.py
@@ -1,23 +1,13 @@
 from .utils import make_request
 
-# created dict for cache data
-user_cache = {}
 
 def user():
-    if 'users' in user_cache:
-        return user_cache['users']
-    else:
-        url = "https://random-data-api.com/api/v2/users"
-        data = make_request(url)
-        
-        
-        user_data = data.copy()
-        keys_to_remove = ["employment", "address", "credit_card", "subscription", "password", "uid"]
-        for key in keys_to_remove:
-            user_data.pop(key, None)
+    url = "https://random-data-api.com/api/v2/users"
+    data = make_request(url)        
+    
+    user_data = data.copy()
+    keys_to_remove = ["employment", "address", "credit_card", "subscription", "password", "uid"]
+    for key in keys_to_remove:
+        user_data.pop(key, None)
 
-        # save in cache
-        user_cache['users'] = user_data
-        
-
-        return user_data
+    return user_data
--- frontend/src/pages/receive_data/components/fetch_data.js
+++ frontend/src/pages/receive_data/components/fetch_data.js
@@ -3,7 +3,11 @@ import axios from "axios";
 export const fetchData = async (set_data_user) => {
     try {
         const response = await axios.get("/receive-data");
-        set_data_user(response.data);
+        if (response.status === 200) {
+            set_data_user(response.data);
+        } else {
+            console.error("Non-success status code:", response.status);
+        }
     } catch (error) {
         console.error("Error fetching data:", error);
     }
