f94e16e84c186a3e52ca1cda03bcb914183d2303
chamois1 <nrabicenko5@gmail.com>
2024-04-07 23:09:54 +0300

    Added on main page link for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ function App() {
     <div className="App">
       <h1>
         Welcome to data management
+
+        <a href="/receive-data-random">Receive random data</a>
+        <a href="/receive-database">Receive data from database</a>
       </h1>
     </div>
   );
